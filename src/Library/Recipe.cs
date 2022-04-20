ac346888a1c71df7dde42432e22b4ee4284b9d96
martino-lucas95 <71530057+martino-lucas95@users.noreply.github.com>
2022-04-20 18:49:36 -0300

    First Commit

---
+++ src/Library/Recipe.cs
@@ -0,0 +1,50 @@
+//-------------------------------------------------------------------------
+// <copyright file="Recipe.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class Recipe
+    {
+        private ArrayList steps = new ArrayList();
+
+        public Product FinalProduct { get; set; }
+
+        public void AddStep(Step step)
+        {
+            this.steps.Add(step);
+        }
+
+        public void RemoveStep(Step step)
+        {
+            this.steps.Remove(step);
+        }
+        /*Utilizo Expert ya que conoce todos los pasos(es la encargada de agregarlos o eliminarlos), obtiene el costo de la clase Step, y con estos datos puede determinar el costo total de produccion e imprimirlo en consola*/
+
+        public double GetProductionCost()
+        {
+            double result = 0;
+
+            foreach (Step step in this.steps)
+            {
+                result = result + step.GetstepCost();
+            }
+
+            return result;
+        }
+        public void PrintRecipe()
+        {
+            Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
+            foreach (Step step in this.steps)
+            {
+                Console.WriteLine($"{step.Quantity} de {step.Input.Description} usando {step.Equipment.Description} durante {step.Time} || Sub-Total: $ {step.GetstepCost()}");
+            }
+            Console.WriteLine($"Costo Total: $ {GetProductionCost()}");
+        }
+    }
+}
---
+++ src/Library/Step.cs
@@ -0,0 +1,32 @@
+//-------------------------------------------------------------------------------
+// <copyright file="Step.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------------
+
+namespace Full_GRASP_And_SOLID.Library
+{
+    public class Step
+    {
+        public Step(Product input, double quantity, Equipment equipment, int time)
+        {
+            this.Quantity = quantity;
+            this.Input = input;
+            this.Time = time;
+            this.Equipment = equipment;
+        }
+
+        /*La clase Step es quien conoce el costo de cada paso, ya que conoce las cantidades/horas y costos por insumo/horas maquina, por lo tanto cumple con el patron EXPERT ya que es quién tiene toda la información necesaria para cumplir con este método.*/
+        public double GetstepCost()
+        {
+            return (this.Input.UnitCost * this.Quantity) + (this.Equipment.HourlyCost * this.Time);
+        }
+        public Product Input { get; set; }
+
+        public double Quantity { get; set; }
+
+        public int Time { get; set; }
+
+        public Equipment Equipment { get; set; }
+    }
+}
